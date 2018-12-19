b0bb7abd6e986cbbab7dfb15c28295c834dbe301
rowKylin <luoyq1234@sina.com>
2018-12-19 23:46:15 +0800

    卡片组件

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,4 +1,30 @@
-const Card = () => (
-    <div>this is a card</div>
-)
-export default Card
+// const Card = () => (
+//     <div>this is a card</div>
+// )
+import Link from 'next/link'
+import componentCss from '../css/componentCss.scss'
+class Cardbox extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {...this.props}
+    }
+    render(){
+        return (
+            <div className={componentCss.cardBox}>
+                <div className={componentCss.cardBoxTitle}>
+                    <div className={componentCss.cardBoxTitleLeft}>this.state.title</div>
+                    {this.state.moreLink?<div className={componentCss.cardBoxTitleRight}>
+                        <Link href={this.state.moreLink}>更多></Link>
+                    </div>:''}
+                </div>
+                <div className={componentCss.cardBoxContent}>
+                    <img src={this.state.contentImg?this.state.contentImg:'/static/images/logo.png'} alt={this.state.contentImgAlt?this.state.contentImgAlt:'东风机械'}/>
+                </div>
+                <div className={componentCss.cardBoxFooter}>
+                    {this.state.footer}
+                </div>
+            </div>
+        )
+    }
+}
+export default Cardbox
