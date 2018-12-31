eb39a32b67f6e8a4c895a1494513c2ef5b3ff07e
rowKylin <luoyq1234@sina.com>
2018-12-31 11:08:37 +0800

    next-redux 12-31

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { viewChange } from '../store'
+import ReduxHandler from '../components/reduxHandler'
+
+
+// import {window} from '../utils/window'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../css/normalize.css'
 import '../css/reset.css'
-const Index = () => (
-    <div>
-        <Header />
-        hello qq
-        <Footer />
-    </div>
-)
+
+class Index extends React.Component{
+    static getInitialProps({reduxStore,req}){
+        const isPc = !!req ? 'pc':'h5'
+        reduxStore.dispatch(viewChange(isPc))
+        return {}
+    }
+
+    componentDidMount(){
+        
+    }
+
+    componentWillUnmount(){
+
+    }
+
+    render(){
+        return (
+        <React.Fragment>
+            <Header></Header>
+            东风机械
+            <ReduxHandler></ReduxHandler>
+            <Footer></Footer>
+        </React.Fragment>
+        )
+    }
+}
+
 export default Index
